6d4c19cab789e359e624c2ad4fccd13f3204b16c
alex <alexandre.piel@gmail.com>
2021-02-02 15:07:23 +0100

    send to midi

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ if (navigator.requestMIDIAccess) {
     alert('No MIDI support in your browser.');
 }
 
+let midi;
+
 function onMIDISuccess(midiAccess) {
+    midi = midiAccess;
     midiAccess.inputs.forEach((midiInput) => {
         console.log('midiInput', midiInput.name, midiInput);
         midiInput.onmidimessage = onMIDIMessage;
+    });
+
+    midiAccess.outputs.forEach((midiOutput, key) => {
+        console.log('midiOutput', midiOutput.name, midiOutput);
+        document.getElementById(
+            'output',
+        ).innerHTML += `<option value="${key}">${midiOutput.name}</option>`;
     });
 }
 
@@ -27,12 +37,42 @@ function onMIDIMessage({ data }) {
     console.log('MIDI data', data);
 }
 
+// send
+
+document.getElementById('note').innerHTML = Array(255)
+    .fill()
+    .map(
+        (_, key) =>
+            `<option value="${(key + 24) % 255}">${(key + 24) % 255}</option>`,
+    )
+    .join('');
+
+document.getElementById('send').onclick = () => {
+    const { value: data } = document.getElementById('data');
+    const { value: key } = document.getElementById('output');
+    const output = midi.outputs.get(key);
+    const msg = JSON.parse(data);
+    console.log(`Send to ${output.name}:`, msg);
+    output.send(msg);
+    // output.send([0x80, 60, 0x40]);
+};
+
+document.getElementById('play').onclick = () => {
+    const { value: note } = document.getElementById('note');
+    const { value: key } = document.getElementById('output');
+    const output = midi.outputs.get(key);
+    console.log(`Play to ${output.name}:`, note);
+    output.send([144, note, 127]);
+    setTimeout(() => output.send([128, note, 127]), 1000);
+};
 
 // Custom console.log
 
 rewireLoggingToElement(
-    () => document.getElementById("log"),
-    () => document.getElementById("log-container"), true);
+    () => document.getElementById('log'),
+    () => document.getElementById('log-container'),
+    true,
+);
 
 function rewireLoggingToElement(eleLocator, eleOverflowLocator, autoScroll) {
     fixLoggingFunc('log');
@@ -43,19 +83,24 @@ function rewireLoggingToElement(eleLocator, eleOverflowLocator, autoScroll) {
 
     function fixLoggingFunc(name) {
         console['old' + name] = console[name];
-        console[name] = function(...arguments) {
+        console[name] = function (...arguments) {
             const output = produceOutput(name, arguments);
             const eleLog = eleLocator();
 
             if (autoScroll) {
                 const eleContainerLog = eleOverflowLocator();
-                const isScrolledToBottom = eleContainerLog.scrollHeight - eleContainerLog.clientHeight <= eleContainerLog.scrollTop + 1;
-                eleLog.innerHTML += output + "<br>";
+                const isScrolledToBottom =
+                    eleContainerLog.scrollHeight -
+                        eleContainerLog.clientHeight <=
+                    eleContainerLog.scrollTop + 1;
+                eleLog.innerHTML += output + '<br>';
                 if (isScrolledToBottom) {
-                    eleContainerLog.scrollTop = eleContainerLog.scrollHeight - eleContainerLog.clientHeight;
+                    eleContainerLog.scrollTop =
+                        eleContainerLog.scrollHeight -
+                        eleContainerLog.clientHeight;
                 }
             } else {
-                eleLog.innerHTML += output + "<br>";
+                eleLog.innerHTML += output + '<br>';
             }
 
             console['old' + name].apply(undefined, arguments);
@@ -64,10 +109,16 @@ function rewireLoggingToElement(eleLocator, eleOverflowLocator, autoScroll) {
 
     function produceOutput(name, args) {
         return args.reduce((output, arg) => {
-            return output +
-                "<span class=\"log-" + (typeof arg) + " log-" + name + "\">" +
-                    (typeof arg === "object" ? JSON.stringify(arg) : arg) +
-                "</span>&nbsp;";
+            return (
+                output +
+                '<span class="log-' +
+                typeof arg +
+                ' log-' +
+                name +
+                '">' +
+                (typeof arg === 'object' ? JSON.stringify(arg) : arg) +
+                '</span>&nbsp;'
+            );
         }, '');
     }
 }
